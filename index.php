0e88a168c67046f701faf9cd41a2e372d2752507
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-07 14:07:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 
 <?php
 echo "My first PHP script!";
-
-<a class="btn btn-primary" href="page1.php" role="button">Go to next page</a>
 ?> 
+<a class="btn btn-primary" href="page1.php" role="button">Go to next page</a>
+
 
 <form action="handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
