b43bd1264b486c39b0d3d08b1a0c9bc243c8f83d
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-07 14:24:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,19 +6,16 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
-  <body>
-    <h1>Hello, world!</h1>
-    
-    <html>
-<body>
-
+ <body>
+  
+<h1>Hello, world!</h1>
 <form action="handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
-</body>
+
     
     <a class="btn btn-primary" href="handlepost.php" role="button">Next Page</a>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
