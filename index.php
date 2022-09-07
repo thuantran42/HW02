f59c4f5cdc5e1198cab2ad0561ec0547b528048f
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-07 16:00:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-
+<html>
+  <body>
 <?php   
 require_once('header.php');
 ?>
@@ -19,4 +20,5 @@ E-mail: <input type="text" name="email"><br>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
- 
+  </body>
+</html>
