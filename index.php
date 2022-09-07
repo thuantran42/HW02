0fe6ea53cd080459605fe2be041528fe3c74c05d
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-07 14:41:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  </head>
- <body>
+<?php   
+require_once('header.php');
+?>
   
 <h1>Hello, world!</h1>
 <form action="handlepost.php" method="post">
@@ -15,10 +9,7 @@ E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
    
-<?php   
-define('__ROOT__', dirname(dirname(__FILE__)));
-require_once(__ROOT__.'header.php');
-?>
+
 
 
     
