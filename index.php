c80f780b0ae1fbe029a4aae97bd00b864540f823
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-09 12:32:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ E-mail: <input type="text" name="email"><br>
 </form>
    
     
-   <a class="btn btn-primary" href="handlepost.php" role="button">Next Page</a>
+   
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
