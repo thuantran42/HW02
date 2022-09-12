c399f3c0984fe68c422453ad5dbe75eb62f9a505
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-12 13:14:47 -0500

    Update welcome_get.php

--- welcome_get.php
+++ welcome_get.php
@@ -3,12 +3,11 @@
   <head>
     Heading <?php require_once('header.php');?>
     
-    Welcome to GET Response <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+    
   </head>
   
 <body>
-
-<a class="btn btn-primary" href="ending.php" role="button">to the last page</a>
+Welcome to GET Response <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
 </body>
 </html>
