4435747461f37f0020e4e5684beb5b4f1b5188e4
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-12 12:46:59 -0500

    Create welcome_get.php

---
+++ welcome_get.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    Welcome to GET Response <?php echo $_GET["What's your name"]; ?><br>
+Your email address is: <?php echo $_GET["What's your email?"]; ?>
+  </head>
+  
+<body>
+
+<a class="btn btn-primary" href="#" role="button">Link</a>
+</body>
+</html>
