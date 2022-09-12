0da90f9bd59414c3eb1858243b21b9bfc906633f
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-12 13:14:54 -0500

    Update ending.php

--- ending.php
+++ ending.php
@@ -2,12 +2,14 @@
 <html>
   <head>
     Heading <?php require_once('header.php');?>
+     
   </head>
   <body>
     <form action="welcome_get.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
+     
 </form>
   </body>
 </html>
