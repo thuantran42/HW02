e1901597894815cc689d7ba135fd168ead2fa997
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-26 14:01:52 -0500

    Update welcome_get.php

--- welcome_get.php
+++ welcome_get.php
@@ -10,4 +10,17 @@
 Welcome to GET Response <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
 </body>
+  <html>
+    <body>
+      
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+     
+</form>
+      
+      
+    </body>
+    <html>
 </html>
