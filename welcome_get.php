4c17a5d1ac808372b346d23593a0a0fa669e0a73
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-26 15:54:47 -0500

    Update welcome_get.php

--- welcome_get.php
+++ welcome_get.php
@@ -1,20 +1,14 @@
-<!DOCTYPE html>
-<html>
-  <head>
+
     Heading <?php require_once('header.php');?>
     
     
-  </head>
-  
-<body>
+
+
 Welcome to GET Response <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
+
+      
+      
+      
 </body>
-  <html>
-    <body>
-      
-      
-      
-    </body>
-    <html>
 </html>
