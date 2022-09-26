fe0d64948b4708d34009126e7ea47c7c4a191e2e
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-09-26 15:57:16 -0500

    Update header.php

--- header.php
+++ header.php
@@ -8,8 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     
 
-  </head>
-  <body>
+  
     <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -46,7 +45,7 @@
     </div>
   </div>
 </nav>
-    
+</head>
   <body>
 
 
